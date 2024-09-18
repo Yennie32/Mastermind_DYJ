c94e011b4e799cd95559a592a6bc46ed88f5a7ef
driss292 <drisskaci@outlook.fr>
2024-09-18 15:55:52 +0200

    Étape 1 OK

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
 //Exercice indiv Mastermind
 // ______ETAPE 1______
-let code = ["rouge", "vert", "bleu", "jaune"]
-let essai1 = ["rouge","jaune"]
-let combinaison=[]
-let couleur1 = prompt("Quelle est la couleur de ton premier pion ?")
-let couleur2 = prompt("Quelle est la couleur de ton second pion ?")
+let couleursAutorisees = ["rouge", "vert", "bleu", "jaune"];
+let combinaison = [];
+let cachee = ["rouge", "jaune"];
+// let couleur1 = prompt("Quelle est la couleur de ton premier pion ?")
+// let couleur2 = prompt("Quelle est la couleur de ton second pion ?")
 
 //VERIFIE QUE LA COULEUR EST AUTORISEE
 function estValide(tableau) {
-    return tableau.every(couleur => code.includes(couleur));
+  return tableau.every((couleur) => couleursAutorisees.includes(couleur));
 }
-console.log(estValide(essai1));
- 
-//COMPARE LE CODE SECRET ET LA PROPOSITION. RETOURNE UN BOOLEEN
-function estCorrect(secret,proposition) {
-    if (secret.length!==proposition.length) return false;
-    console.log(JSON.stringify(secret ));
-    
-    return JSON.stringify(secret)===JSON.stringify(proposition)
-}
-console.log(estCorrect(essai1, combinaison));
+// console.log(estValide(essai1));
 
+//COMPARE LE CODE SECRET ET LA PROPOSITION. RETOURNE UN BOOLEEN
+function estCorrect(secret, proposition) {
+  if (secret.length !== proposition.length) return false;
+
+  return JSON.stringify(secret) === JSON.stringify(proposition);
+}
+// console.log(estCorrect(essai1, combinaison));
 
 // RECUPERE INPUT DE L'UTILISATEUR ET LE TABLIFIE
-function essaiCouleur () {
-    combinaison.push(couleur1, couleur2)
-    return combinaison
+function essaiCouleur() {
+  let couleur1 = prompt("Couleur 1");
+  let couleur2 = prompt("Couleur 2");
+  combinaison.push(couleur1, couleur2);
+  return combinaison;
 }
-console.log(essaiCouleur());
-
 
 // INITIALISE LE NOMBRE DE TENTATIVES
-const tentativesMax = 12
-let tentatives = 0
 
 // GERE LE GAMEPLAY
 function partieDeJeu() {
-    let devineCouleur =  essaiCouleur()
-    while (tentatives <= tentativesMax || estCorrect(essai1, devineCouleur) === false){
-        if(!estValide(devineCouleur)){
-            alert("Tu dois choisir une couleur autorisée") 
-        }  else {
-            estCorrect(essai1, devineCouleur)
-            tentatives ++
-            console.log(tentatives);
-            console.log(estCorrect(essai1, devineCouleur));     
-        } if (tentatives === tentativesMax){
-            alert("You lose, sucker !")
-        } if (estCorrect(essai1, devineCouleur) ===true){
-            alert("That's a good boy :)")
+  let tentativesMax = 3;
+  let trouvé = false;
 
-        }
-    break
+  let devineCouleur = essaiCouleur();
+  tentativesMax--;
+
+  while (tentativesMax > 0 && !trouvé) {
+    if (!estValide(devineCouleur)) {
+      console.log("INVALIDE");
+      devineCouleur = essaiCouleur();
+      continue;
     }
+    let tentative = estCorrect(cachee, devineCouleur);
+    if (!tentative) {
+      console.log("Try Again");
+      tentativesMax--;
+      devineCouleur = essaiCouleur();
+    } else {
+      console.log("Bravo");
+      trouvé = true;
+    }
+  }
 }
 
-partieDeJeu()
-
-
-
-// let tableau1 = ["a", "b", "c", "d"]
-// let tableau2 = ["a", "b", "3", "4"]
-
-// let intersection = tableau1.filter(val => tableau2.includes(val))
-
-// console.log(intersection)
-
-//  ["a", "b"]
+partieDeJeu();
