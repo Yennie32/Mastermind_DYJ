9cd0b86db82003fc59fb8090d9078d335f89ccf6
Your Name <you@example.com>
2024-09-19 10:50:11 +0200

    to fix : condition de fin et add index couleur

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 //Exercice indiv Mastermind
+// to do : drag and drop ??
+import shuffleColor from "./test.js";
+let texte = document.querySelector("#info");
 // ______ETAPE 1______
-const couleursAutorisees = [
-  "rouge",
-  "vert",
-  "bleu",
-  "jaune",
-  "rose",
-  "violet",
-  "blanc",
-  "orange",
-];
+let couleursAutorisees = {
+  1: "rouge",
+  2: "vert",
+  3: "bleu",
+  4: "jaune",
+  5: "rose",
+  6: "violet",
+  7: "blanc",
+  8: "orange",
+};
 let combinaison = [];
-let cachee = ["rouge", "jaune"];
+let cachee = shuffleColor(couleursAutorisees);
+console.log(cachee);
+
 // let couleur1 = prompt("Quelle est la couleur de ton premier pion ?")
 // let couleur2 = prompt("Quelle est la couleur de ton second pion ?")
 
-//VERIFIE QUE LA COULEUR EST AUTORISEE
-function estValide(tableau) {
-  return tableau.every((couleur) => couleursAutorisees.includes(couleur));
-}
-// console.log(estValide(essai1));
+// //VERIFIE QUE LA COULEUR EST AUTORISEE
+// function estValide(tableau) {
+//   return tableau.every((couleur) => couleursAutorisees.includes(couleur));
+// }
+// // console.log(estValide(essai1));
 
 //COMPARE LE CODE SECRET ET LA PROPOSITION. RETOURNE UN BOOLEEN
 function estCorrect(secret, proposition) {
@@ -28,13 +33,18 @@ function estCorrect(secret, proposition) {
   return JSON.stringify(secret) === JSON.stringify(proposition);
 }
 // console.log(estCorrect(essai1, combinaison));
-
+let bouton = document.querySelector("#button")
+bouton.addEventListener("click", partieDeJeu)
 // RECUPERE INPUT DE L'UTILISATEUR ET LE TABLIFIE
 function essaiCouleur() {
-  let couleur1 = prompt("Couleur 1");
-  let couleur2 = prompt("Couleur 2");
-  combinaison.push(couleur1, couleur2);
-  return combinaison;
+  let tabCouleur = [];
+  let choixCouleur = document.querySelectorAll(".couleur")
+  console.log(choixCouleur.value);
+  choixCouleur.forEach(couleur => {
+    tabCouleur.push(couleur.value);
+  })
+  console.log("tabCouleur", tabCouleur);
+  return tabCouleur;
 }
 
 // INITIALISE LE NOMBRE DE TENTATIVES
@@ -48,21 +58,25 @@ function partieDeJeu() {
   tentativesMax--;
 
   while (tentativesMax > 0 && !trouvé) {
-    if (!estValide(devineCouleur)) {
-      console.log("INVALIDE");
-      devineCouleur = essaiCouleur();
-      continue;
-    }
+    // if (!estValide(devineCouleur)) {
+    //   console.log("INVALIDE");
+    //   devineCouleur = essaiCouleur();
+    //   continue;
+    // }
     let tentative = estCorrect(cachee, devineCouleur);
     if (!tentative) {
-      console.log("Try Again");
+      texte.innerText = "Essaie encore..."
       tentativesMax--;
       devineCouleur = essaiCouleur();
+
     } else {
-      console.log("Bravo");
+      texte.innerText = "Bravo"
       trouvé = true;
-    }
-  }
+    }   
+  }  
+   if (!trouvé) {
+    texte.innerText = "C'est perdu..."
+    return
+  } 
 }
 
-partieDeJeu();
--- test.js
+++ test.js
@@ -1,13 +1,5 @@
-let couleursAutorisees = {
-  1: "rouge",
-  2: "vert",
-  3: "bleu",
-  4: "jaune",
-  5: "rose",
-  6: "violet",
-  7: "blanc",
-  8: "orange",
-};
+
+
 // POKER
 // function shuffleArray(array) {
 //   for (let i = array.length - 1; i >= 0; i--) {
@@ -18,7 +10,7 @@ let couleursAutorisees = {
 // }
 // console.log(shuffleArray(couleursAutorisees));
 
-function shuffleColor(obj) {
+export default function shuffleColor(obj) {
   let array = [];
   for (let i = 0; i < 4; i++) {
     let random = Math.floor(Math.random() * 8) + 1;
@@ -26,7 +18,6 @@ function shuffleColor(obj) {
   }
   return array;
 }
-console.log(shuffleColor(couleursAutorisees));
 
 // let randomNumber = Math.floor(Math.random() * 8) + 1;
 // console.log(randomNumber);
